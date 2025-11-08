a795b130a272b3a7c45d7d97fd3d22738289d95a
3bslam1 <omarmahmoudabdellslam@gmail.com>
2025-11-08 15:39:51 +0200

    Fix image path\sss

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
